5290069648a9cec9b6dd48bd87eb919292876846
iErnesto <ernesto1991diaz@gmail.com>
2023-01-15 17:33:15 -0600

    Create close auctions function

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,8 +1,21 @@
+import { closeAuction } from '../../lib/closeAuction';
 import { getEndedAuctions } from '../../lib/getEndedAuctions';
+import createError from 'http-errors';
 
 async function processAuctions(event, context) {
-  const auctionsToClose = await getEndedAuctions();
-  console.log(auctionsToClose);
+  try {
+    const auctionsToClose = await getEndedAuctions();
+    const closePromises = auctionsToClose.map((auction) =>
+      closeAuction(auction)
+    );
+
+    await Promise.all(closePromises);
+
+    return { closed: closePromises.length };
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 }
 
 export const handler = processAuctions;

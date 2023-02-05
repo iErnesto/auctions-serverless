d64571432fe68b0fee5ef0090a34aa177ab89bba
iErnesto <ernesto1991diaz@gmail.com>
2023-02-04 22:23:59 -0600

    Add error handling to upload picture function

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,8 +1,30 @@
+import middy from '@middy/core';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors';
+import { getAuctionById } from './getAuction';
+import { uploadPictureToS3 } from '../../lib/uploadPictureToS3';
+
 export async function uploadAuctionPicture(event) {
+  const { id } = event.pathParameters;
+  const auction = await getAuctionById(id);
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
+  const buffer = Buffer.from(base64, 'base64');
+
+  try {
+    const uploadToS3Result = await uploadPictureToS3(
+      auction.id + '.jpg',
+      buffer
+    );
+    console.log(uploadToS3Result);
+  } catch (error) {
+    console.error(error);
+  throw new  createError.InternalServerError(error)
+  
+
   return {
     statusCode: 200,
     body: JSON.stringify({}),
   };
 }
 
-export const handler = uploadAuctionPicture;
+export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
